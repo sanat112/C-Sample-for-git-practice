3ea012396f6efa7b29a94574ae439f08c574e9ef
Sanat <sanat@DESKTOP-R2EMSST.localdomain>
2025-06-24 11:42:50 +0530

    sum function added

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
+int sum(int a,int b);
 int main()
 {
 int a=5;
 printf("----------welcome to Git-----------\n");
-printf("%d\n",a);
+printf("sum:%d\n",sum(5,3));
 return 0;
+}
+int sum(int a,int b)
+{
+int result;
+result=a+b;
+return result;
 }
